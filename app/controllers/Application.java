03373e67ea8f018498186fc172f8ed3df5597ce9
brentmy <brentmy@gmail.com>
2013-10-06 11:55:08 -1000

    BASE project

---
+++ app/controllers/Application.java
@@ -0,0 +1,58 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.Index;
+import views.html.Page1;
+import views.html.carissa;
+import views.html.jake;
+import views.html.mick;
+
+/**
+ * Implements the controllers for this application.
+ */
+public class Application extends Controller {
+
+  /**
+   * Returns the home page. 
+   * @return The resulting home page. 
+   */
+  public static Result index() {
+    return ok(Index.render("Welcome to the home page."));
+  }
+  
+  /**
+   * Returns page1, a simple example of a second page to illustrate navigation.
+   * @return The Page1.
+   */
+  public static Result page1() {
+    return ok(Page1.render("Welcome to Page1."));
+    
+  }
+  
+  /**
+   * Returns page1, a simple example of a second page to illustrate navigation.
+   * @return The Page1.
+   */
+  public static Result carissa() {
+    return ok(carissa.render("Welcome to Page1."));
+    
+  }
+  
+  /**
+   * Returns page1, a simple example of a second page to illustrate navigation.
+   * @return The Page1.
+   */
+  public static Result jake() {
+    return ok(jake.render("Welcome to Page1."));
+    
+  }
+  /**
+   * Returns page1, a simple example of a second page to illustrate navigation.
+   * @return The Page1.
+   */
+  public static Result mick() {
+    return ok(mick.render("Welcome to Page1."));
+    
+  }
+}
