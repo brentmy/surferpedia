59c1ba42631253eee1ca9035ba81ce01acb6eae7
MattCCieslak <dechtmatt@gmail.com>
2013-10-09 11:03:21 -1000

    Added Jeff Hakman

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,6 +9,7 @@ import views.html.jake;
 import views.html.mick;
 import views.html.Kanoa;
 import views.html.Megan;
+import views.html.jeff;
 
 /**
  * Implements the controllers for this application.
@@ -16,57 +17,73 @@ import views.html.Megan;
 public class Application extends Controller {
 
   /**
-   * Returns the home page. 
-   * @return The resulting home page. 
+   * Returns the home page.
+   * 
+   * @return The resulting home page.
    */
   public static Result index() {
     return ok(Index.render("Welcome to the home page."));
   }
-  
+
   /**
    * Returns page1, a simple example of a second page to illustrate navigation.
+   * 
    * @return The Page1.
    */
   public static Result page1() {
     return ok(Page1.render("Welcome to Page1."));
-    
+
   }
-  
+
   /**
    * Returns page1, a simple example of a second page to illustrate navigation.
+   * 
    * @return The Page1.
    */
   public static Result carissa() {
     return ok(carissa.render("Welcome to Page1."));
-    
+
   }
-  
+
   /**
    * Returns page1, a simple example of a second page to illustrate navigation.
+   * 
    * @return The Page1.
    */
   public static Result jake() {
     return ok(jake.render("Welcome to Page1."));
-    
+
   }
+
   /**
    * Returns page1, a simple example of a second page to illustrate navigation.
+   * 
    * @return The Page1.
    */
   public static Result mick() {
     return ok(mick.render("Welcome to Page1."));
   }
-  
+
   /**
    * Returns page1, a simple example of a second page to illustrate navigation.
+   * 
    * @return The Page1.
    */
   public static Result kanoa() {
     return ok(Kanoa.render("Welcome to Kanoa's page."));
   }
-  
+
   public static Result Megan() {
     return ok(Megan.render(""));
-    
   }
+
+  /**
+   * Returns Jeff's page.
+   * 
+   * @return Jeff's page.
+   */
+  public static Result jeff() {
+    return ok(jeff.render("Welcome to Jeff's page."));
+  }
+
 }
